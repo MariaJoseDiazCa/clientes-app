58bd0710e27ae430619f59d26093a35ff9fe4e6e
MariaJoseDiazCa <mariajose.diazca@gmail.com>
2022-06-08 15:13:25 -0400

    ADD: Clientes component

---
+++ src/app/clientes/clientes.json.ts
@@ -0,0 +1,15 @@
+import { Cliente} from './cliente';
+
+export const CLIENTES: Cliente[] = [
+  {id: 1,nombre: 'María José', apellido: 'Díaz', email: 'mjdiazc@hotmail.com',createAt: '2022-01-12'},
+  {id: 2,nombre: 'Natalí', apellido: 'Díaz', email: 'natidiazc@hotmail.com',createAt: '2022-03-03'},
+  {id: 3,nombre: 'Juliette', apellido: 'Díaz', email: 'juliettediazc@hotmail.com',createAt: '2022-05-21'},
+  {id: 4,nombre: 'Pablo', apellido: 'Carvajal', email: 'pabloC@hotmail.com',createAt: '2021-11-03'},
+  {id: 5,nombre: 'Paula', apellido: 'Gonzalez', email: 'PaulaGON@hotmail.com',createAt: '2021-07-09'},
+  {id: 6,nombre: 'Cristian', apellido: 'Ramos', email: 'CC@hotmail.com',createAt: '2021-12-08'},
+  {id: 7,nombre: 'Irnma', apellido: 'Carvajal', email: 'irmaC@hotmail.com',createAt: '2021-07-02'},
+  {id: 8,nombre: 'Patricia', apellido: 'Carvajal', email: 'patriciaC@hotmail.com',createAt: '2021-05-05'},
+  {id: 9,nombre: 'Daniela', apellido: 'Naranjo', email: 'Daniela@hotmail.com',createAt: '2021-06-11'},
+  {id: 10,nombre: 'Camila', apellido: 'Navarrete', email: 'navarrete@hotmail.com',createAt: '2021-09-11'},
+  {id:11,nombre: 'Javier', apellido: 'Urrutia', email: 'Javielo@hotmail.com',createAt: '2021-12-13'}
+];
