976e2443e52261a851fa7f9d5be5e76fbdc5b682
MariaJoseDiazCa <mariajose.diazca@gmail.com>
2022-07-27 18:57:18 -0400

    ADD: HttpClient

--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -2,15 +2,18 @@ import { Injectable } from '@angular/core';
 import { Cliente} from './cliente';
 import {CLIENTES } from './clientes.json'
 import { Observable, of } from 'rxjs';
+import {HttpClient} from '@angular/common/http';
+import {map} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClienteService {
+  private urlEndPoint: string = 'http://localhost:8080/api/clientes';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   getClientes(): Observable<Cliente[]> {
-    return of(CLIENTES);
+    return this.http.get<Cliente[]>(this.urlEndPoint);
   }
 }
