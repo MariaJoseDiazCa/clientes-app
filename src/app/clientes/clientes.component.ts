f0321f16bb91e9a0fd09722203e8fad703117dac
MariaJoseDiazCa <mariajose.diazca@gmail.com>
2022-08-04 16:21:17 -0400

    ADD: delete client

--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Cliente} from './cliente';
 import { ClienteService } from './cliente.service'
+import  swal from 'sweetalert2'
 
 @Component({
   selector: 'app-clientes',
@@ -16,6 +17,31 @@ export class ClientesComponent implements OnInit {
      this.clienteService.getClientes().subscribe(
        clientes => this.clientes = clientes
      );
+  }
+
+  delete(cliente: Cliente): void {
+    swal({
+      title: '¿Está seguro?',
+      text: `¿Desea borrar al cliente ${cliente.nombre} ${cliente.apellido}?`,
+      type: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Si, eliminar!',
+      cancelButtonText: 'No, cancelar!',
+      confirmButtonClass: 'btn btn-success',
+      cancelButtonClass: 'btn btn-danger',
+      reverseButtons: true,
+      buttonsStyling: false
+    }).then((result) => {
+      if (result.value) {
+
+        this.clienteService.delete(cliente.id).subscribe( response => {
+              this.clientes = this.clientes.filter(clie => clie !== cliente);
+              swal('Borrado!','Your file has been deleted.','success');
+         });
+      }
+    });
   }
 
 }
