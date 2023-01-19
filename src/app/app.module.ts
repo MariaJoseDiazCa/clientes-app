5293c33b9ec3b2de6a7be60a262bb07273f5a6d4
MariaJoseDiazCa <mariajose.diazca@gmail.com>
2023-01-19 13:41:09 -0300

    ADD: DatePicker component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,32 +1,37 @@
-import { NgModule, LOCALE_ID } from '@angular/core';
+import { NgModule, LOCALE_ID, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component'
+import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { DirectivaComponent } from './directiva/directiva.component';
 import { ClientesComponent } from './clientes/clientes.component';
 import { FormComponent } from './clientes/form.component';
 import { PaginatorComponent } from './paginator/paginator.component';
-import { ClienteService} from './clientes/cliente.service';
+import { ClienteService } from './clientes/cliente.service';
 import { RouterModule, Routes } from '@angular/router';
-import {HttpClientModule} from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
-import {registerLocaleData} from '@angular/common';
+import { registerLocaleData } from '@angular/common';
 import localeES from '@angular/common/locales/es-CL';
-
-
+import { MatMomentDateModule } from '@angular/material-moment-adapter';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import * as moment from 'moment';
 
 registerLocaleData(localeES, 'cl');
 
+// moment.es.ts created previously
+moment.locale('es');
+
 const routes: Routes = [
-  { path: '', redirectTo: '/clientes', pathMatch: 'full'},
-  { path: 'directivas', component: DirectivaComponent},
-  { path: 'clientes', component: ClientesComponent},
-  { path: 'clientes/page/:page', component: ClientesComponent},
-  { path: 'clientes/form', component: FormComponent},
-  { path: 'clientes/form/:id', component: FormComponent},
-]
+  { path: '', redirectTo: '/clientes', pathMatch: 'full' },
+  { path: 'directivas', component: DirectivaComponent },
+  { path: 'clientes', component: ClientesComponent },
+  { path: 'clientes/page/:page', component: ClientesComponent },
+  { path: 'clientes/form', component: FormComponent },
+  { path: 'clientes/form/:id', component: FormComponent },
+];
 @NgModule({
   declarations: [
     AppComponent,
@@ -35,15 +40,23 @@ const routes: Routes = [
     DirectivaComponent,
     ClientesComponent,
     FormComponent,
-    PaginatorComponent
+    PaginatorComponent,
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     FormsModule,
-    RouterModule.forRoot(routes)
+    MatMomentDateModule,
+    MatDatepickerModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(routes),
   ],
-  providers: [ClienteService, {provide: LOCALE_ID, useValue:'cl'}],
-  bootstrap: [AppComponent]
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  providers: [
+    ClienteService,
+    { provide: LOCALE_ID, useValue: 'cl' },
+    MatDatepickerModule,
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
